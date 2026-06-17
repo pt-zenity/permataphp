41ea0ca163f5173fcfb4acc605bacab750d40787
pt-zenity <genspark_dev@genspark.ai>
2026-06-17 11:36:05 +0000

    Remove .gitignore - push semua file termasuk assist.ini.php

---
+++ include/assist.ini.php
@@ -0,0 +1,24 @@
+# assist.ini.php - Konfigurasi koneksi database
+# Edit nilai di bawah ini sesuai environment produksi
+# Baris dimulai dengan # adalah komentar (diabaikan)
+
+# Hostname / IP database server
+ip=localhost
+
+# Port database (default 3306)
+port=3306
+
+# Nama database
+database=bpr_web
+
+# Username database
+user=Assist
+
+# Password database  
+password=Irac
+
+# URL Assist Pro (opsional)
+# urlassistpro=http://localhost/assist-pro
+
+# URL Assist Auth (opsional)
+# urlassistauth=http://localhost/assist-auth
